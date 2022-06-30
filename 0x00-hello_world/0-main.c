edd935c9866b2a3f368d558fea38ecc90165180b
Wondemu Woyo <wondewoyo2013@gmail.com>
2022-06-30 06:59:42 -0700

    preproc

--- 0x00-hello_world/0-main.c
+++ 0x00-hello_world/0-main.c
@@ -16,6 +16,8 @@ int main(void)
   
 {
   
+  printf("Holberton School");
+  
   return (0);
   
 }
--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+include <stdio.h>
 
 
 
@@ -13,8 +13,10 @@
  */
 
 int main(void)
-  
+
 {
+  
+  printf("Holberton School");
   
   return (0);
   
